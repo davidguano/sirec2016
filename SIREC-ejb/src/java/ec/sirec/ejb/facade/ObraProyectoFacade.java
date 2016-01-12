169db676a07561f067afb85ca7b1d9264a683844
new <new@new-PC>
2016-01-11 20:08:57 -0500

    ok

--- SIREC-ejb/src/java/ec/sirec/ejb/facade/ObraProyectoFacade.java
+++ SIREC-ejb/src/java/ec/sirec/ejb/facade/ObraProyectoFacade.java
@@ -6,10 +6,13 @@
 
 package ec.sirec.ejb.facade;
 
+import ec.sirec.ejb.entidades.CatastroPredial;
 import ec.sirec.ejb.entidades.ObraProyecto;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -27,6 +30,26 @@ public class ObraProyectoFacade extends AbstractFacade<ObraProyecto> {
 
     public ObraProyectoFacade() {
         super(ObraProyecto.class);
+    }
+    
+    
+  public ObraProyecto buscarValorMejora(CatastroPredial vvalor1) throws Exception {
+        String sql = " select p from Mejora m, ObraProyecto p "
+                + " where m.catpreCodigo=:vvalor1 and "
+                + " m.obrCodigo=p.obrCodigo";                        
+        Query q = getEntityManager().createQuery(sql);
+        q.setParameter("vvalor1", vvalor1);
+        List<ObraProyecto> resultado = q.getResultList();
+        if (resultado.size() > 0) {
+            return (ObraProyecto) resultado.get(0);
+        } else {
+            return null;
+        }                      
+       // postgres 
+//       SELECT *
+//  FROM sirec.mejora m, sirec.obra_proyecto p
+//  where m.catpre_codigo='21' and
+//  m.obr_codigo= p.obr_codigo;       
     }
     
 }
