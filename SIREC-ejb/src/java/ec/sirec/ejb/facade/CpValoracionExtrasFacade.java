474afe97e66a46bb53b8c3c2659fc8adcecc5d8d
new <new@new-PC>
2016-01-21 00:03:40 -0500

    ok

--- SIREC-ejb/src/java/ec/sirec/ejb/facade/CpValoracionExtrasFacade.java
+++ SIREC-ejb/src/java/ec/sirec/ejb/facade/CpValoracionExtrasFacade.java
@@ -53,19 +53,29 @@ public class CpValoracionExtrasFacade extends AbstractFacade<CpValoracionExtras>
 //  d.adided_codigo=e.adided_codigo and
 //  e.catpreval_codigo=5;
     
-     public BigDecimal obteneValorTipoAdicional(Object vvalor1, Object vvalor2, Object vvalor3) throws Exception {
+     public BigDecimal obteneValorTipoAdicional(Object vvalor1, Object vvalor4, Object vvalor2, Object vvalor3) throws Exception {
         String sql = " select sum(e.cpvalextValor) from CatastroPredial c, CatastroPredialValoracion v, CpValoracionExtras e, AdicionalesDeductivos d"
                 + " where c.catpreCodigo=:vvalor1 and "
                 + " c.catpreCodigo=v.catpreCodigo and "
                 + " v.catprevalCodigo=e.catprevalCodigo and "
                 + " d.adidedCodigo=e.adidedCodigo and "
                 + " d.adidedTipoImpuesto=:vvalor2 and "
-                + " d.adidedTipo=:vvalor3";                        
+                + " d.adidedTipo=:vvalor3 and "                        
+                + " v.catprevalCodigo=:vvalor4";                        
         Query q = getEntityManager().createQuery(sql);
         q.setParameter("vvalor1", vvalor1);
         q.setParameter("vvalor2", vvalor2);
         q.setParameter("vvalor3", vvalor3);
+        q.setParameter("vvalor4", vvalor4);
         return (BigDecimal)q.getSingleResult();
     } 
-    
+//    SELECT  sum(e.cpvalext_valor)
+//  FROM sirec.catastro_predial c, sirec.catastro_predial_valoracion v, sirec.cp_valoracion_extras e, sirec.adicionales_deductivos d
+//  where c.catpre_codigo=21 and 
+//  c.catpre_codigo=v.catpre_codigo and 
+//  v.catpreval_codigo=e.catpreval_codigo and 
+//  d.adided_codigo=e.adided_codigo and
+//  d.adided_tipo_impuesto='PR' and
+//  d.adided_tipo='R' and
+//     v.catpreval_codigo=5;;
 }
